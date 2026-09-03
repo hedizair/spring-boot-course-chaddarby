43bf4f911530ecca1b1ee831d5e62eb60bdd3fbe
hedizair <hedi.marwan@hotmail.fr>
2026-09-03 16:39:07 +0200

    Finalize section 04-04 - Spring Data REST completed

--- 04-04-section-4-rest-crud-employee-with-data-rest/src/main/java/com/hzair/springboot/cruddemo/CruddemoApplication.java
+++ 04-04-section-4-rest-crud-employee-with-data-rest/src/main/java/com/hzair/springboot/cruddemo/CruddemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CruddemoApplication {
 
+	// * No need to do controllers and services because we use Spring Data REST and
+	// * Spring Data JPA to handles Rest CRUD operations automatically.
 	public static void main(String[] args) {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
--- 04-04-section-4-rest-crud-employee-with-data-rest/src/main/java/com/hzair/springboot/cruddemo/dao/EmployeeRepository.java
+++ 04-04-section-4-rest-crud-employee-with-data-rest/src/main/java/com/hzair/springboot/cruddemo/dao/EmployeeRepository.java
@@ -7,6 +7,8 @@ import com.hzair.springboot.cruddemo.entity.Employee;
 // * JpaRepository is the implementation of Spring Data JPA
 // * It provides CRUD operations for the Employee entity Autmmatically
 // * We don't need to implement DAO code anymore .
+
+// * @RepositoryRestResource(path="members") // * This annotation is used to customize the REST endpoint path for the Employee entity.
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> { // * We need to specify the entity type "Employee" and the primary key type "Integer".
     
 }
