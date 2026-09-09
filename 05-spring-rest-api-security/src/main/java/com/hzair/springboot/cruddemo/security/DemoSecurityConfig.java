e8a9edb8df2f3ea0f28490bb856124986aff9a66
hedizair <hedi.marwan@hotmail.fr>
2026-09-09 14:56:30 +0200

    finalize section 5 : Spring Security

--- 05-spring-rest-api-security/src/main/java/com/hzair/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-rest-api-security/src/main/java/com/hzair/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,14 +1,15 @@
 package com.hzair.springboot.cruddemo.security;
 
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.hzair.springboot.cruddemo.CruddemoApplication;
@@ -22,29 +23,50 @@ public class DemoSecurityConfig {
         this.cruddemoApplication = cruddemoApplication;
     }
 
+
+    // * Add support for JDBC, no more hardcoded users in memory.
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() { //* Create In Memory user with their roles and password
+    public UserDetailsManager userDetailsManager(DataSource dataSource) { // * DataSource auto injected by spring boot
 
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123") // * {noop} is used to indicate that the password is stored in plain text and no encoding is applied.
-                .roles("EMPLOYEE")
-                .build();
+        // * If custom tables are provided, here is the code to tell Spring Security to use them.
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+        // * Define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("SELECT user_id, pw, active from members WHERE user_id = ?");
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+        // * Define a qyery to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT user_id, role from roles WHERE user_id = ?");
 
-        return new InMemoryUserDetailsManager(john, mary, susan);
+        return jdbcUserDetailsManager;
+        // ------------------------------------------
+
+        // * If no custom tables are provided, Spring Security will use the default tables (users & authorities), no need to do anything.
+        // return new JdbcUserDetailsManager(dataSource); // * Tell Spring Security to use JDBC authentication with the provided DataSource
     }
+
+    // @Bean
+    // public InMemoryUserDetailsManager userDetailsManager() { //* Create In Memory user with their roles and password
+
+    //     UserDetails john = User.builder()
+    //             .username("john")
+    //             .password("{noop}test123") // * {noop} is used to indicate that the password is stored in plain text and no encoding is applied.
+    //             .roles("EMPLOYEE")
+    //             .build();
+
+    //     UserDetails mary = User.builder()
+    //             .username("mary")
+    //             .password("{noop}test123")
+    //             .roles("EMPLOYEE", "MANAGER")
+    //             .build();
+
+    //     UserDetails susan = User.builder()
+    //             .username("susan")
+    //             .password("{noop}test123")
+    //             .roles("EMPLOYEE", "MANAGER", "ADMIN")
+    //             .build();
+
+    //     return new InMemoryUserDetailsManager(john, mary, susan);
+    // }
 
     @Bean 
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { // * Configure each roles authorization for each HTTP method and endpoint
@@ -55,6 +77,7 @@ public class DemoSecurityConfig {
                 .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
                 .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
                 .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
         );
 
         // * Use HTTP Basic authentication
