25b8dbb59a66648b7f00baddbc27816bf7cd04ed
hedizair <hedi.marwan@hotmail.fr>
2026-09-10 18:40:01 +0200

    finalize section 7 : MVC with CRUD operations

--- 07-section-7-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
+++ 07-section-7-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -1,12 +1,13 @@
-package com.luv2code.springboot.cruddemo.service;
-
-import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
-import com.luv2code.springboot.cruddemo.entity.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+package com.luv2code.springboot.thymeleafdemo.service;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luv2code.springboot.thymeleafdemo.dao.EmployeeRepository;
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -20,7 +21,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public List<Employee> findAll() {
-        return employeeRepository.findAll();
+        return employeeRepository.findAllByOrderByLastNameAsc();
     }
 
     @Override
